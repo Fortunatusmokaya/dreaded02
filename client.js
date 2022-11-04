5fd94f0a509d2e9f8872be80181dffafc79d0fd7
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2022-11-04 13:35:43 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,146 @@
+const { useSingleFileAuthState, DisconnectReason, makeInMemoryStore, msgRetryCounterMap, delay } = require('baileys')
+const session = process.argv[2] ? process.argv[2] + '.json' : 'session.json'
+const { state, saveState } = useSingleFileAuthState(session)
+const pino = require('pino'), path = require('path'), fs = require('fs'), colors = require('@colors/colors/safe'), qrcode = require('qrcode-terminal')
+const spinnies = new (require('spinnies'))()
+const { Socket, Serialize, Scandir } = require('./system/extra')
+global.props = new (require('./system/dataset'))
+global.neoxr = new (require('./system/map'))
+global.Func = new (require('./system/function'))
+global.scrap = new (require('./system/scraper'))
+global.store = makeInMemoryStore({
+   logger: pino().child({
+      level: 'silent',
+      stream: 'store'
+   })
+})
+
+const connect = async () => {
+   global.db = {users:{}, chats:{}, groups:{}, statistic:{}, sticker:{}, setting:{}, ...(await props.fetch() ||{})}
+   await props.save(global.db)
+   global.client = Socket({
+      logger: pino({
+         level: 'silent'
+      }),
+      printQRInTerminal: true,
+      msgRetryCounterMap,
+      browser: ['dreaded/ fortunatus', 'safari', '1.0.0'],
+      auth: state,
+      // To see the latest version : https://web.whatsapp.com/check-update?version=1&platform=web
+      version: [2, 2240, 7],
+      getMessage: async (key) => {
+         return await store.loadMessage(client.decodeJid(key.remoteJid), key.id)
+      }
+   })
+
+   store.bind(client.ev)
+   client.ev.on('connection.update', async (update) => {
+      const {
+         connection,
+         lastDisconnect,
+         qr
+      } = update
+      if (lastDisconnect == 'undefined' && qr != 'undefined') {
+         qrcode.generate(qr, {
+            small: true
+         })
+      }
+      if (connection === 'connecting') spinnies.add('start', {
+         text: 'Connecting . . .'
+      })
+      if (connection === 'open') {
+         spinnies.succeed('start', {
+            text: `Connected, you login as ${client.user.name || client.user.verifiedName}`
+         })
+      }
+      if (connection === 'close') {
+         if (lastDisconnect.error.output.statusCode == DisconnectReason.loggedOut) {
+            spinnies.fail('start', {
+               text: `Can't connect to Web Socket`
+            })
+            await props.save()
+            process.exit(0)
+         } else {
+            connect().catch(() => connect())
+         }
+      }
+   })
+
+   client.ev.on('creds.update', () => saveState)
+
+   client.ev.on('messages.upsert', async chatUpdate => {
+      try {
+         m = chatUpdate.messages[0]
+         if (!m.message) return
+         Serialize(client, m)
+         Scandir('./plugins').then(files => {
+            global.client.plugins = Object.fromEntries(files.filter(v => v.endsWith('.js')).map(file => [path.basename(file).replace('.js', ''), require(file)]))
+         }).catch(e => console.error(e))
+         require('./system/config'), require('./handler')(client, m)
+      } catch (e) {
+         console.log(e)
+      }
+   })
+
+   client.ev.on('contacts.update', update => {
+      for (let contact of update) {
+         let id = client.decodeJid(contact.id)
+         if (store && store.contacts) store.contacts[id] = {
+            id,
+            name: contact.notify
+         }
+      }
+   })
+
+   client.ev.on('group-participants.update', async (room) => {
+      let meta = await (await client.groupMetadata(room.id))
+      let member = room.participants[0]
+      let text_welcome = `Thank you +tag for joining into +grup group.`
+      let text_left = `+tag has left 🤧`
+      let groupSet = global.db.groups[room.id]
+      try {
+         pic = await Func.fetchBuffer(await client.profilePictureUrl(member, 'image'))
+      } catch {
+         pic = await Func.fetchBuffer(await client.profilePictureUrl(room.id, 'image'))
+      }
+      if (room.action == 'add') {
+         if (groupSet.localonly) {
+            if (typeof global.db.users[member] != 'undefined' && !global.db.users[member].whitelist && !member.startsWith('254') || !member.startsWith('254')) {
+               client.reply(room.id, Func.texted('bold', `Sorry @${member.split`@`[0]}, this group is only for Kenyan people. Good Bye...`))
+               client.updateBlockStatus(member, 'block')
+               return await Func.delay(2000).then(() => client.groupParticipantsUpdate(room.id, [member], 'remove'))
+            }
+         }
+         let txt = (groupSet.text_welcome != '' ? groupSet.text_welcome : text_welcome).replace('+tag', `@${member.split`@`[0]}`).replace('+grup', `${meta.subject}`)
+         if (groupSet.welcome) client.sendMessageModify(room.id, txt, null, {
+            largeThumb: true,
+            thumbnail: pic,
+            url: global.db.setting.link
+         })
+      } else if (room.action == 'remove') {
+         let txt = (groupSet.text_left != '' ? groupSet.text_left : text_left).replace('+tag', `@${member.split`@`[0]}`).replace('+grup', `${meta.subject}`)
+         if (groupSet.left) client.sendMessageModify(room.id, txt, null, {
+            largeThumb: true,
+            thumbnail: pic,
+            url: global.db.setting.link
+         })
+      }
+   })
+
+   client.ws.on('CB:call', async json => {
+      if (json.content[0].tag == 'offer') {
+         let object = json.content[0].attrs['call-creator']
+         await Func.delay(2000)
+      users.banned = true
+         await client.updateBlockStatus(object, 'block')
+      }
+   })
+
+   setInterval(async () => {
+      if (global.db) await props.save()
+   }, 10_000)
+
+   return client
+}
+
+connect().catch(() => connect())
