7cc077b79edcec54e0b3c502a5a1f0b8d75cc2da
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2022-11-04 14:01:14 +0300

    Update restrict.js

--- plugins/event/system/restrict.js
+++ plugins/event/system/restrict.js
@@ -8,8 +8,8 @@ exports.run = {
    }) => {
       try {
          if (!m.fromMe && isOwner) return
-    	 // if (/(save|sv)/.test(body)) return client.reply(m.chat, `Males, gak penting!`, m).then(async () => await client.updateBlockStatus(m.sender, 'block'))
-         // if (body.toUpperCase() == 'P') return client.reply(m.chat, `PaPePaPe kalo chat orang itu yang bener Tolol!`, m).then(async () => await client.updateBlockStatus(m.sender, 'block'))
+    	 // if (/(save|sv)/.test(body)) return client.reply(m.chat, `🗿`, m).then(async () => await client.updateBlockStatus(m.sender, 'block'))
+         // if (body.toUpperCase() == 'P') return client.reply(m.chat, `🗿`, m).then(async () => await client.updateBlockStatus(m.sender, 'block'))
       } catch (e) {
          console.log(e)
       }
