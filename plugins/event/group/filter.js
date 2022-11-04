b44e60c02c4a0214bac067fbad3b34f233e32e6a
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2022-11-04 13:57:08 +0300

    Add files via upload

--- plugins/event/group/filter.js
+++ plugins/event/group/filter.js
@@ -1,1 +1,45 @@
-
+exports.run = {
+   async: async (m, {
+      client,
+      body,
+      users,
+      groupSet,
+      setting,
+      isAdmin,
+      isBotAdmin
+   }) => {
+      try {
+         if (groupSet.filter && !isAdmin && isBotAdmin && !m.fromMe) {
+            let toxic = setting.toxic
+            if (body && (new RegExp('\\b' + toxic.join('\\b|\\b') + '\\b')).test(body.toLowerCase())) {
+               groupSet.member[m.sender].warning += 1
+               let warning = groupSet.member[m.sender].warning
+               if (warning > 1) return client.reply(m.chat, Func.texted(`bold`, `They never listen. 🤧`), m).then(() => {
+                  client.groupParticipantsUpdate(m.chat, [m.sender], 'remove').then(async () => {
+                     groupSet.member[m.sender].warning = 0
+                 client.sendMessage(m.chat, {
+                        delete: {
+                           remoteJid: m.chat,
+                           fromMe: isBotAdmin ? false : true,
+                           id: m.key.id,
+                           participant: m.sender
+                        }
+                     })
+                  })
+               })
+               return client.sendMessage(m.chat, {
+                  delete: {
+                     remoteJid: m.chat,
+                     fromMe: isBotAdmin ? false : true,
+                     id: m.key.id,
+                     participant: m.sender
+                  }
+               }).then(() => client.reply(m.chat, `❗  D E T E C T E D \n\nThat was a bad word. Bot will kick you next without prior warning! 🗿`, m))
+            }
+         }
+      } catch (e) {
+         return client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   group: true
+}
